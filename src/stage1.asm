2aff8d4036bf3a5a5b30411d5630fb3117388483
aczech17 <adrian-czech@outlook.com>
2025-01-30 23:06:22 +0100

    force reboot if error

--- src/stage1.asm
+++ src/stage1.asm
@@ -1,26 +1,22 @@
 %include "out/sectors.inc" ; count of sectors of the stage2
 
 [org 0x7C00]
-jmp word 0x0000:start ; ???
+jmp word 0x0000:start ; Make sure we have address 0x0000:0x7C00, NOT 0x07C0:0x0000.
 
 start:
-	; Initialize the stack.
-	;mov bp, 0x8000
-	;mov sp, bp
-
-	; Now try to read the 2nd sector of the disk.
+	; Read the 2nd stage of the bootloader from the 2nd sector of the disk.
 
 	; read the sector to 0x2000:0x0000
 	mov ax, 0x2000
 	mov es, ax
 	mov bx, 0
 
-	; drive number in DL already set by BIOS
-	mov ah, 2	; 2 read sector BIOS routine
+	; Drive number in DL already set by BIOS.
+	mov ah, 2				; 2 read sector BIOS routine
 	mov al, SECTORS_TO_READ
-	mov ch, 0	; cylinder 0
-	mov cl, 2	; sector 2 (they start from 1)
-	mov dh, 0	; head number 0 (they start from 0(wtf?))
+	mov ch, 0				; cylinder 0
+	mov cl, 2				; sector 2 (they start from 1)
+	mov dh, 0				; head number 0, they start from 0 (sic!)
 	
 	int 13h
 	jc error
@@ -29,6 +25,7 @@ start:
 	jmp word 0x2000:0x0000
 
 	; The CPU should never reach here.
+	jmp 0xFFFF:0x0000 ; Force reboot if it reaches here after all.
 
 error:
 	mov bx, error_message
