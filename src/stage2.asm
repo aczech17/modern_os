6e72df3cea600427672754d8431cbecf893379da
aczech17 <adrian-czech@outlook.com>
2025-01-30 20:44:28 +0100

    switch to 32 bit

--- src/stage2.asm
+++ src/stage2.asm
@@ -1,23 +1,30 @@
+[bits 16]
 [org 0x0000]
     
     cli	; disable the interrupts because 16-bit interrupt vector will be invalid in 32 bit
-	lgdt [gdt_descriptor]
+
+	; set up segment registers
+	mov ax, 0x2000
+	mov ds, ax
+	mov es, ax
+
+
+	lgdt [gdt_32_addr]
 
 	; set 0 bit in the CR0
 	mov eax, cr0
 	or eax, 0x01
 	mov cr0, eax
-jmp $
-	jmp CODE_SEG:start_protected_mode
+
+	jmp dword 0x8:(0x20000+start_protected_mode)
 
 	[bits 32]
 start_protected_mode:
+
 	; set up the segment registers
-	mov ax, DATA_SEG
+	mov ax, 0x10
 	mov ds, ax
 	mov es, ax
-	mov fs, ax
-	mov gs, ax
 	mov ss, ax
 
 	; set up the stack
@@ -32,35 +39,29 @@ start_protected_mode:
 
 %include "src/print_string_vga.asm"
 
-gdt_start:
+gdt_32_addr:
+	dw (gdt_32.end - gdt_32) - 1
+	dd 0x20000 + gdt_32
 
-gdt_null:
+;times (32 - ($ - $$) % 32) db 0xcc
+
+gdt_32:
+.null:
 	dd 0
 	dd 0
-gdt_code:
-	dw 0xFFFF 			; limit (bits 0-15)
-	dw 0x0000 			; base (bits 0-15)
-	db 0x0				; base (bits 16-23)
-	db 1_00_1_1010b 	; 1 st flags, type flags
-	db 1_1_0_0_1111b	; 2 nd flags, Limit (bits 16-19)
-	db 0x0				; Base (bits 24-31)
+.code:
+	dd 0xffff  ; segment limit
+	dd (10 << 8) | (1 << 12) | (1 << 15) | (0xf << 16) | (1 << 22) | (1 << 23)
 
-gdt_data:
-	dw 0xFFFF 			; limit (bits 0-15)
-	dw 0x0000 			; base (bits 0-15)
-	db 0x0				; base (bits 16-23)
-	db 1_00_1_0010b 	; 1 st flags, type flags
-	db 1_1_0_0_1111b	; 2 nd flags, Limit (bits 16-19)
-	db 0x0				; Base (bits 24-31)
-gdt_end:
-
-gdt_descriptor:
-	dw gdt_end - gdt_start - 1 ; GDT size - always less one of the true size
-	dd gdt_start
-
-; constants for indexing segments int the GDT
-CODE_SEG equ gdt_code - gdt_start
-DATA_SEG equ gdt_data - gdt_start
+.data:
+	dd 0xffff  ; segment limit
+	dd (2 << 8) | (1 << 12) | (1 << 15) | (0xf << 16) | (1 << 22) | (1 << 23)
+.null_2:
+	dd 0
+	dd 0
+.end:
 
 
-hello_message db "Switched to 32 bit.", 0
+hello_message db 'witched to 32 bit.', 0
+
+;times (512 - ($ - $$) % 512) db 0
