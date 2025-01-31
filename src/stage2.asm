00cdf13e191cfbb9a2d31e3aa43ccbaa0cd9dfa6
aczech17 <adrian-czech@outlook.com>
2025-01-31 17:33:46 +0100

    jump to C

--- src/stage2.asm
+++ src/stage2.asm
@@ -39,27 +39,27 @@ start_32:
 	mov es, ax
 	mov ss, ax
 
-; set_up_page_tables:
-; 	mov eax, page_table.level3 + 0x20000
-; 	or eax, 0b11 ; present + writable
-; 	mov [page_table.level4 + 0x20000], eax
+set_up_page_tables:
+	mov eax, page_table.level3 + 0x20000
+	or eax, 0b11 ; present + writable
+	mov [page_table.level4 + 0x20000], eax
 
-; 	mov eax, page_table.level2 + 0x20000
-; 	or eax, 0b11 ; present + writable
-; 	mov [page_table.level3 + 0x20000], eax
+	mov eax, page_table.level2 + 0x20000
+	or eax, 0b11 ; present + writable
+	mov [page_table.level3 + 0x20000], eax
 
-; 	; Map each level2 entry to a huge 2MiB page.
-; 	mov ecx, 0
-; .map_p2_table:
-; 	; Map ecx-th P2 entry to a huge page that starts at address 2MiB * ECX.
-; 	mov eax, 1 << 21 ; 2MiB
-; 	mul ecx ; start address of ECX-th page
-; 	or eax, 0b10000011 ; huge + present + writable
-; 	mov [0x20000 + page_table.level2 + ecx * 8], eax ; map ECX-th entry
+	; Map each level2 entry to a huge 2MiB page.
+	mov ecx, 0
+.map_p2_table:
+	; Map ecx-th P2 entry to a huge page that starts at address 2MiB * ECX.
+	mov eax, 1 << 21 ; 2MiB
+	mul ecx ; start address of ECX-th page
+	or eax, 0b10000011 ; huge + present + writable
+	mov [0x20000 + page_table.level2 + ecx * 8], eax ; map ECX-th entry
 
-; 	inc ecx
-; 	cmp ecx, 512
-; 	jne .map_p2_table
+	inc ecx
+	cmp ecx, 512
+	jne .map_p2_table
 
 enable_paging:
 	; Load level4 table to CR3 register.
@@ -147,7 +147,7 @@ kernel_load:
 	mov rsi, rbp	
 
 .next:
-	add rsi, 0x38	; 0x20 == ph table entry size
+	add rsi, 0x38
 	dec rcx
 	jnz .ph_loop
 
@@ -209,31 +209,13 @@ gdt_64:
 
 align 4096
 
-; page_table:
-; .level4:
-; 	times 512 dq 0
-; .level3:
-; 	times 512 dq 0
-; .level2:
-; 	times 512 dq 0
-
-
 page_table:
 .level4:
-	dq 1 | (1 << 1) | (.level3 - $$ + 0x20000)
-	times 511 dq 0x0
+	times 512 dq 0
 .level3:
-	dq 1 | (1 << 1) | (.level2 - $$ + 0x20000)
-	times 511 dq 0x0
+	times 512 dq 0
 .level2:
-	; preprocessor loop dance... - mapping first 1GB == 512 * 0x200000 bytes
-	; 0x83 == 0x1 + 0x2 + 0x80 ---> Present/Writeablei/2MB page
-	%assign i 0
-	%rep 512
-	dq 0x200000*i+0x83
-	%assign i i+1
-	%endrep
-	
+	times 512 dq 0
 
 times (512 - ($ - $$) % 512) db 0
 kernel:
