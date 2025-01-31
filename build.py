1ee401a0c4ecc9b32e1919aec65e1ee515338ab1
aczech17 <adrian-czech@outlook.com>
2025-01-31 07:27:03 +0100

    python build script

---
+++ build.py
@@ -0,0 +1,65 @@
+import os
+import subprocess
+import sys
+
+def assemble_asm(source, output):
+    print(f"Compiling {source}.")
+    subprocess.run(['nasm', source, '-o', output], check=True)
+
+
+def calculate_sectors(file_paths):
+    all_file_sizes = 0
+    for file_path in file_paths:
+        if not os.path.isfile(file_path):
+            print(f"Error: File {file_path} not found!")
+            exit(1)
+        file_size = os.path.getsize(file_path)
+        all_file_sizes += file_size
+
+
+    sectors = (all_file_sizes + 511) // 512  # Round up to 512.
+    print(f"Sectors to read from disk: {sectors}")
+
+    with open('out/sectors.inc', 'w') as f:
+        f.write(f"SECTORS_TO_READ equ {sectors}\n")
+
+    print("SECTORS_TO_READ written to out/sectors.inc")
+
+def create_disk_image():
+    print("Joining object files...")
+    with open("out/os.img", "wb") as img_file:
+        # Łączenie stage1.o i stage2.o w jeden plik obrazu
+        with open("out/stage1.o", "rb") as stage1:
+            img_file.write(stage1.read())
+        with open("out/stage2.o", "rb") as stage2:
+            img_file.write(stage2.read())
+
+def run_qemu():
+    subprocess.run(['qemu-system-x86_64', '-drive', 'format=raw,file=out/os.img', '-m', '2048'])
+
+def clean_all():
+    subprocess.run(['rm', '-rf', 'out'])
+
+def main():
+    if len(sys.argv) > 1 and sys.argv[1] == "clean":
+        clean_all()
+        return
+
+    os.makedirs('out', exist_ok=True)
+
+    # Assemble stage 2
+    assemble_asm('src/stage2.asm', 'out/stage2.o')
+
+    calculate_sectors(['out/stage2.o'])
+
+    # Assemble stage 1
+    assemble_asm('src/stage1.asm', 'out/stage1.o')
+
+
+    create_disk_image()
+
+    run_qemu()
+
+# Uruchomienie programu
+if __name__ == '__main__':
+    main()
--- calculate_sectors.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-FILE_SIZE=$(stat -c%s "$1")  # Pobierz rozmiar pliku w bajtach
-SECTORS=$(( (FILE_SIZE + 511) / 512 ))  # Zaokrąglij w górę do pełnego sektora
-echo "SECTORS_TO_READ equ $SECTORS" > out/sectors.inc
