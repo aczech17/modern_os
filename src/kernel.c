78617511f10d76d8003a55c7314127cbb576579f
aczech17 <adrian-czech@outlook.com>
2025-02-01 04:44:38 +0100

    testing VGA

--- src/kernel.c
+++ src/kernel.c
@@ -13,11 +13,21 @@ void kernel_main(u64 kernel_address)
     };
 
     clear_screen(&vga_buffer);
-    write_string(&vga_buffer, "Booting Modern OS...\n");
+    write_string(&vga_buffer, "Super system kurwo\n");
 
-    const char* kernel_location_msg = "Kernel loaded to address: ";
-    write_string(&vga_buffer, kernel_location_msg);
+    vga_buffer.color = 0x14;
+    write_string(&vga_buffer, "Brawo, Robercik!\n");
+
+    vga_buffer.color = 0x4e;
+    write_string(&vga_buffer, "Hej, Jaga gol!\n");
+
+    vga_buffer.color = 0x07;
+    write_string(&vga_buffer, "Kernel jest pod adresem ");
     write_hex(&vga_buffer, kernel_address, false);
 
-    for (;;);
+    write_string(&vga_buffer, ",\nczyli po ludzku ");
+    write_dec(&vga_buffer, kernel_address);
+    write_string(&vga_buffer, ".\n");
+
+    // for (;;);
 }
