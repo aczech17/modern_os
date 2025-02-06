e0965a9d7c090b1253c34d487312743f5b6345f9
aczech17 <adrian-czech@outlook.com>
2025-02-06 02:02:48 +0100

    added partition table

--- src/boot/stage1.asm
+++ src/boot/stage1.asm
@@ -5,7 +5,7 @@ jmp word 0x0000:start ; Make sure we have address 0x0000:0x7C00, NOT 0x07C0:0x00
 
 start:
 	; Read the 2nd stage of the bootloader from the 2nd sector of the disk.
-
+	
 	; read the sector to 0x2000:0x0000
 	mov ax, 0x2000
 	mov es, ax
@@ -27,6 +27,9 @@ start:
 	; The CPU should never reach here.
 	jmp 0xFFFF:0x0000 ; Force reboot if it reaches here after all.
 
+
+
+
 error:
 	mov bx, error_message
 	mov ah, 0x0E
@@ -43,6 +46,21 @@ error:
 
 
 error_message: db "Error reading disk.", 0
+
+times 446 - ($ - $$) db 0
+partition_table:
+    db  0x80  ; bootable
+    db  0x00  ; Head start
+    db  0x01  ; Cylinder/Sector start
+    db  0x00  ; Cylinder start (MSB)
+    db  0x0C  ; FAT32 LBA
+    db  0xFE  ; Head end
+    db  0xFF  ; Cylinder/Sector end
+    db  0xFF  ; Cylinder end (MSB)
+    dd  0x00000001  ; LBA start partition (first sector after MBR)
+    dd  0xFFFFFFFF  ; Size of partition in sectors (maximum).
+
+    times 48 db 0  ; Null partitions (3 partitions 16B each)
 
 times 510 - ($ - $$) db 0
 db 0x55
