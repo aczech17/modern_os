5aca4e5c77d7dcc32a0e0a09f4425c9937361aa4
aczech17 <adrian-czech@outlook.com>
2025-02-10 03:26:21 +0100

    set origin 0x20000 in bootloader stage2

--- src/boot/stage2.asm
+++ src/boot/stage2.asm
@@ -1,5 +1,5 @@
 [bits 16]
-[org 0x0000]
+[org 0x20000]
 stage2:
 	; Enable A20 http://wiki.osdev.org/A20_Line
 	mov ax, 0x2401
@@ -8,7 +8,6 @@ stage2:
 	in al, 0x92
 	or al, 2
 	out 0x92, al
-
 
     cli ; Disable the interrupts because 16-bit interrupt vector will be invalid in 32 bit.
 
@@ -29,7 +28,7 @@ stage2:
 	mov cr0, eax
 
 	; Far jump to clean the pipeline and finally start the 32 bit mode.
-	jmp dword 0x8:(0x20000+start_32)
+	jmp dword 0x8:start_32
 
 	[bits 32]
 start_32:
@@ -40,13 +39,13 @@ start_32:
 	mov ss, ax
 
 set_up_page_tables:
-	mov eax, page_table.level3 + 0x20000
+	mov eax, page_table.level3
 	or eax, 0b11 ; present + writable
-	mov [page_table.level4 + 0x20000], eax
+	mov [page_table.level4], eax
 
-	mov eax, page_table.level2 + 0x20000
+	mov eax, page_table.level2
 	or eax, 0b11 ; present + writable
-	mov [page_table.level3 + 0x20000], eax
+	mov [page_table.level3], eax
 
 	; Map each level2 entry to a huge 2MiB page.
 	mov ecx, 0
@@ -55,7 +54,7 @@ set_up_page_tables:
 	mov eax, 1 << 21 ; 2MiB
 	mul ecx ; start address of ECX-th page
 	or eax, 0b10000011 ; huge + present + writable
-	mov [0x20000 + page_table.level2 + ecx * 8], eax ; map ECX-th entry
+	mov [page_table.level2 + ecx * 8], eax ; map ECX-th entry
 
 	inc ecx
 	cmp ecx, 512
@@ -63,7 +62,7 @@ set_up_page_tables:
 
 enable_paging:
 	; Load level4 table to CR3 register.
-	mov eax, page_table.level4 + 0x20000
+	mov eax, page_table.level4
 	mov cr3, eax
 
 	; Enable PAE flag in CR4 (physical address extension).
@@ -82,9 +81,8 @@ enable_paging:
 	or eax, 1 << 31
 	mov cr0, eax
 
-
-	lgdt [gdt_64_addr + 0x20000]
-	jmp dword 0x8:(0x20000 + start_64)
+	lgdt [gdt_64_addr]
+	jmp dword 0x8:start_64
 
 	[bits 64]
 start_64:
@@ -97,12 +95,12 @@ start_64:
     
 	; Now let's load the kernel.
 kernel_load:
-	mov rsi, [0x20000 + kernel + 0x20]			; Load e_phof -- start of the program header table.
+	mov rsi, [kernel + 0x20]			; Load e_phof -- start of the program header table.
 												; Now in RSI we have the offset from the start of the kernel file,
 												; but we want the offset in the memory, so
-	add rsi, 0x20000 + kernel					; we add to RSI the size of the stage 2.
+	add rsi, kernel					; we add to RSI the size of the stage 2.
 
-	movzx ecx, word [0x20000 + kernel + 0x38]	; Load e_phnum -- the number of entries in the program header table.
+	movzx ecx, word [kernel + 0x38]	; Load e_phnum -- the number of entries in the program header table.
 
 	cld									; Clear direction flag for future rep movsb.
 	mov r14, 0	; Kernel start address  -- first PT_LOAD v_addr. At first, it's 0 (unset).
@@ -131,7 +129,7 @@ kernel_load:
 	mov al, 0
 	rep stosb
 
-	lea rsi, [0x20000 + kernel + r8d]	; source (kernel start + p_offset)
+	lea rsi, [kernel + r8d]	; source (kernel start + p_offset)
 	mov rdi, r9							; destination  (p_vaddr)
 	mov rcx, r10						; count of bytes (p_filesz)
 	rep movsb
@@ -147,21 +145,21 @@ kernel_load:
 
 
 set_up_stack:
-	mov rbp, stack_bottom + 0x20000
+	mov rbp, stack_bottom
 	mov rsp, rbp
-	 
 
 jump_to_kernel:
-	mov rdi, r14						; Give kernel start address to the kernel.
-	mov rax, [0x20000 + kernel + 0x18] 	; e_entry -- entry point
-	call rax							; Finally jump to the kernel.
-
+	mov rdi, r14				; Give kernel start address to the kernel.
+	mov rax, [kernel + 0x18]	; e_entry -- entry point
+	call rax					; Finally jump to the kernel.
 
 	jmp $
 
+
+
 gdt_32_addr:
 	dw (gdt_32.end - gdt_32) - 1
-	dd 0x20000 + gdt_32
+	dd gdt_32
 align 32
 
 gdt_32:
@@ -183,7 +181,7 @@ gdt_32:
 
 gdt_64_addr:
 	dw (gdt_64.end - gdt_64) - 1
-	dd 0x20000 + gdt_64
+	dd gdt_64
 align 32
 
 gdt_64:
