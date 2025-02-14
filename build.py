4f4928785e909a502b7be0a9cc287d45e879eee0
aczech17 <adrian-czech@outlook.com>
2025-02-14 05:36:32 +0100

    load kernel to 1MiB

--- build.py
+++ build.py
@@ -4,44 +4,34 @@ import sys
 
 bootloader_sources = ['src/boot/stage1.asm', 'src/boot/stage2.asm']
 kernel_sources = ['src/kernel.c', 'src/vga.c', 'src/common.c']
-
+linker_script = 'linker.ld'
 
 def assemble_asm(source, output):
     print(f"Assembling {source}.")
     subprocess.run(['nasm', source, '-o', output], check=True)
 
-def compile_kernel(source_files, output):
-    print(f"Compiling {', '.join(source_files)} to {output}.")
-    subprocess.run(['gcc', '-nostdlib', '-mno-red-zone', '-Wall', '-Wextra', *source_files, '-e', 'kernel_main', '-o', output], check=True)
-    subprocess.run(['strip', output])
+def compile_kernel(source_files, output, linker_script):
+    print(f"Compiling {', '.join(source_files)} with linker script {linker_script} to {output}.")
+    subprocess.run([
+    'gcc', '-ffreestanding', '-nostartfiles', '-mno-red-zone', '-Wall', '-Wextra', '-s',
+    *source_files, '-T', linker_script, '-e', 'kernel_main', '-o', output
+    ], check=True)
 
 def calculate_sectors(file_paths):
-    all_file_sizes = 0
-    for file_path in file_paths:
-        if not os.path.isfile(file_path):
-            print(f"Error: File {file_path} not found!")
-            exit(1)
-        file_size = os.path.getsize(file_path)
-        all_file_sizes += file_size
-
+    all_file_sizes = sum(os.path.getsize(f) for f in file_paths if os.path.isfile(f))
     print(f"Stage 2 and kernel size is {all_file_sizes} B.")
-    sectors = (all_file_sizes + 511) // 512  # Round up to 512.
+    sectors = (all_file_sizes + 511) // 512
     print(f"That adds up to {sectors} sectors.")
-
     with open('out/sectors.inc', 'w') as f:
         f.write(f"SECTORS_TO_READ equ {sectors}\n")
-
     print("SECTORS_TO_READ written to out/sectors.inc")
 
 def create_disk_image():
     print("Joining the OS files...")
     with open("out/os.img", "wb") as img_file:
-        with open("out/stage1.bin", "rb") as stage1:
-            img_file.write(stage1.read())
-        with open("out/stage2.bin", "rb") as stage2:
-            img_file.write(stage2.read())
-        with open("out/kernel.elf", "rb") as kernel:
-            img_file.write(kernel.read())
+        for filename in ["out/stage1.bin", "out/stage2.bin", "out/kernel.elf"]:
+            with open(filename, "rb") as f:
+                img_file.write(f.read())
 
 def run_qemu():
     subprocess.run(['qemu-system-x86_64', '-drive', 'format=raw,file=out/os.img', '-m', '4G'])
@@ -68,22 +58,16 @@ def main():
         input("Press any key to compile though.")
         print()
 
-
     os.makedirs('out', exist_ok=True)
 
-    # Assemble stage 2 and compile the kernel.
     assemble_asm(bootloader_sources[1], 'out/stage2.bin')
-    compile_kernel(kernel_sources, 'out/kernel.elf')
+    compile_kernel(kernel_sources, 'out/kernel.elf', linker_script)
 
-    # Calculate how many sectors do they take.
     calculate_sectors(['out/stage2.bin', 'out/kernel.elf'])
 
-    # Assemble stage 1
     assemble_asm(bootloader_sources[0], 'out/stage1.bin')
-
-    # Join the OS files to the final OS image.
+    
     create_disk_image()
-
     print_success_message()
 
     if len(sys.argv) > 1 and sys.argv[1] == "run":
--- src/kernel.c
+++ src/kernel.c
@@ -1,12 +1,44 @@
+/*
+    VGA colors
+    (background << 4) | foreground
+    0x0	Black	    0x8	Dark Gray
+    0x1	Blue	    0x9	Light Blue
+    0x2	Green	    0xa	Light Green
+    0x3	Cyan	    0xb	Light Cyan
+    0x4	Red	        0xc	Light Red
+    0x5	Magenta	    0xd	Pink
+    0x6	Brown	    0xe	Yellow
+    0x7	Light Gray	0xf	White
+*/
+
 #include "vga.h"
 #include <stdbool.h>
+
+
+void print_memory_size(u64 memsize)
+{
+    const char* units[] = {"B", "KiB", "MiB", "GiB", "TiB", "PiB"};
+    size_t len = 6;
+
+    for (size_t i = 0; i < len; ++i)
+    {
+        if ((memsize >> 10) == 0)
+        {
+            print("%z%u%s", 0x0A, memsize, units[i]);
+            break;
+        }
+
+        memsize = memsize >> 10;
+    }
+}
 
 void kernel_main(u64 mmap_addr, u32 mmap_count, u64 ph_addr, u16 ph_count)
 {
     clear_screen(0x07);
     print("%zSuper system kurwo!\n\n", 0x4E);
     
-    print("Dostepne sekcje pamieci:\n");
+    u64 total_memory_available = 0;
+    print("Physical memory sections:\n");
     for (u32 i = 0; i < mmap_count; ++i)
     {
         u64 base = *(u64*)(mmap_addr + i * 24);
@@ -19,10 +51,18 @@ void kernel_main(u64 mmap_addr, u32 mmap_count, u64 ph_addr, u16 ph_count)
             continue;
 
         u64 end = base + size - 1;
-        print("base = %X, end = %X, type = %X\n", base, end, type);
-    }
+        print("base = %X, end = %X\n", base, end);
 
-    print("\nKernel zajmuje sekcje:\n");
+        if (base > 0)
+            total_memory_available += size;
+    }
+    print("%zTotal memory available ~= ", 0x0A);
+    print_memory_size(total_memory_available);
+    print("\n");
+
+    // print("%u\n", total_memory_available);
+
+    print("\nKernel sections:\n");
     for (u16 i = 0; i < ph_count; ++i)
     {
         u32 p_type = *(u32*)(ph_addr + i * 0x38);
@@ -33,7 +73,6 @@ void kernel_main(u64 mmap_addr, u32 mmap_count, u64 ph_addr, u16 ph_count)
         u64 p_memsz = *(u64*)(ph_addr + i * 0x38 + 0x28);
         u64 end = v_addr + p_memsz - 1;
         print("start = %X, end = %X\n", v_addr, end);
-
     }
     
     for (;;);
